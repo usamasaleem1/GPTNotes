7a23db879897d6d6878083ec0defe659aac0401e
Usama Saleem <usama.saleem@hotmail.com>
2023-03-21 14:59:45 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,72 +1,24 @@
-import { Configuration, OpenAIApi } from "openai";
+import openai from "openai";
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-export default async function(req, res) {
-    const completion = await openai.createCompletion("text-davinci-003", {
-        prompt: generatePrompt(req.body.animal),
-        temperature: 0.3,
-        max_tokens: 700,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text, loading: false });
+async function generatePrompt(animal) {
+  const question = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `I am a highly intelligent bot that can generate lecture notes for you. Given the lecture slides, I will create lecture notes for you.\n\nQ: ${question}\nA: `;
 }
 
-function generatePrompt(animal) {
-    const question =
-        animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-    return `I am a highly intelligent question answering bot that knows a lot about Islam and the Quran. If you ask me a question that is rooted in truth, I will give you the answer along with it's proof with a snippet from the Quran and where it can be found. I will make sure that the reference in the Quran is referring to the question asked specifically, and not just a random quote from the Quran. If it is not found in the Quran, I will say add that this is referenced outside of the Quran and potentially from a Hadith. If I know the answer but its not written in the Quran, I will say so. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Sorry brah, idk the answer to that. Could be because the answer isn't found in the Quran, but maybe in a hadith. Or did you ask an islamic question?".
+export default async function (req, res) {
+  const response = await openai.ChatCompletion.create({
+    engine: "gpt-3.5-turbo",
+    prompt: generatePrompt(req.body.animal),
+    max_tokens: 700,
+    temperature: 0.3,
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "What are the main points of the lecture?" },
+    ],
+  });
 
-Q: If im a Christian am I going to hell?
-A: No. Allah states that as long as you believe in him, whether its in a different religion, you will not need to worry. This is stated in Chapter 2, Verse 62: "Lo! Those who believe (in that which is revealed unto thee, Muhammad), and those who are Jews, and christians, and Sabaeans - whoever believeth in Allah and the Last Day and doeth right - surely their reward is with their Lord, and there shall no fear come upon them neither shall they grieve."
-
-Q: Can I remarry right away after my divorce if im a woman?
-A: You need to wait a period of about 3 months before you can. Allah says in the Quran, Chapter 2, Verse 228: "Women who are divorced shall wait, keeping themselves apart, three (monthly) courses. And it is not lawful for them that they should conceal that which Allah hath created in their wombs if they are believers in Allah and the Last Day. And their husbands would do better to take them back in that case if they desire a reconciliation. And they (women) have rights similar to those (of men) over them in kindness, and men are a degree above them. Allah is Mighty, Wise."
-
-Q: Can I eat pork?
-A: No. Eating pork is forbidden in Islam. This is stated in Chapter 2, Verse 173: "He hath forbidden you only carrion, and blood, and swineflesh, and that which hath been immolated to (the name of) any other than Allah. But he who is driven by necessity, neither craving nor transgressing, it is no sin for him. Lo! Allah is Forgiving, Merciful."
-
-Q: How is a baby developed in the womb according to the Quran?
-A: According to the Quran, a baby is developed in stages in the womb. This is stated in Chapter 23, Verse 14: "Then We made the drop into a clot of congealed blood; then of that clot We made a (foetus) lump; then we made out of that lump bones and clothed the bones with flesh; then we developed out of it another creature. So blessed be Allah, the best to create!"
-
-Q: How was the universe created according to the quran?
-A: According to the Quran, the universe was created in six days. This is stated in Chapter 7, Verse 54: "Your Guardian-Lord is Allah, Who created the heavens and the earth in six days, and is firmly established on the throne (of authority)."
-
-Q: What is the meaning of life, according to the quran?
-A: According to the Quran, the meaning of life is to worship Allah and to live a life of righteousness. This is stated in Chapter 51, Verse 56: "And I (Allah) created not the jinn and mankind except that they should worship Me (Alone)."
-
-Q: Give me motivation to not sin
-A: Keep in mind the consequences of your actions and the fact that Allah is watching you. Remember that Shaytan is behind driving you to sin. Allah mentions this in chapter 5, verse 91: “Satan’s plan is to incite enmity and hatred among you with intoxicants and gambling, and hinder you from the remembrance of Allah and regular prayer. Will you not then abstain?”
-
-Q: Can I shave my beard
-A: Yes, though it is recomended you grow out your beard.
-
-Q: Is it acceptable to drink alcohol?
-A: No, consuming alcohol is strictly prohibited in Islam.
-
-Q: Can I have multiple wives?
-A: Polygamy (having multiple wives) is allowed in Islam, but it is not recommended and requires the husband to treat all wives justly and equally.
-
-Q: Is it mandatory to pray 5 times a day?
-A: Yes, the 5 daily prayers are one of the 5 pillars of Islam.
-
-Q: Can I wear jewelry?
-A: Yes, wearing jewelry is allowed in Islam as long as it is not extravagant and does not cause harm to oneself or others.
-
-Q: Is gambling allowed in Islam?
-A: No, gambling is considered haram in Islam as it involves risk and uncertainty, and can lead to financial ruin.
-
-Q: Is it okay to have a tattoo?
-A: Tattoos are generally considered haram in Islam as they involve permanently altering the body.
-
-Q: Is it necessary to fast during the month of Ramadan?
-A: Yes, fasting during the month of Ramadan is one of the 5 pillars of Islam and is mandatory for all adult Muslims who are physically able.
-
-Q: Is it okay to have a mixed-gender gathering?
-A: It is allowed to have mixed-gender gatherings in Islam as long as they are conducted in a respectful and modest manner. However, it is recommended to have separate spaces for men and women to ensure modesty and privacy.
-
-Q: ${question}
-A: `;
+  res.status(200).json({
+    result: response.choices[0].text,
+    loading: false,
+  });
 }
